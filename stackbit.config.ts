cdc638c7cb7763e1d44d1fb35424631cfa74a4a5
Dario Santos <dariosantos@MacBook-Pro-von-Dario-2.local>
2025-03-28 05:09:25 +0100

    fix: corrected stackbit config

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,7 +1,14 @@
 import { defineStackbitConfig } from '@stackbit/types';
 
 export default defineStackbitConfig({
-  devCommand: "npm run dev",
-  nodeVersion: "18"
+  stackbitVersion: "~0.6.0",
+  nodeVersion: "18",
+  ssgName: "custom",
+  contentSources: [],
+  postInstallCommand: "npm i --no-save @stackbit/types",
+  devCommand: "npm run dev"
 });
+
+
+
 
